b714b8093786128af189b64254309809a2ec9992
Rananga <rananga@gmail.com>
2015-10-24 12:19:34 +0530

    C1

--- app/src/main/java/lk/gadmei/hemantha/gadmei/MainActivity.java
+++ app/src/main/java/lk/gadmei/hemantha/gadmei/MainActivity.java
@@ -6,15 +6,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,22 +29,29 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
 
             myWebView = (WebView) findViewById(R.id.webView);
-            final ProgressBar Pbar = (ProgressBar) findViewById(R.id.pBar);
-            final TextView txtview = (TextView) findViewById(R.id.tV1);
+
+            final LinearLayout loMain = (LinearLayout) findViewById(R.id.loMain);
+            final LinearLayout loLeft = (LinearLayout) findViewById(R.id.loLeft);
+            final FrameLayout loRight = (FrameLayout) findViewById(R.id.loRight);
+            final FrameLayout loInnerLeft = (FrameLayout) findViewById(R.id.loInnerLeft);
+            final FrameLayout loInnerRight = (FrameLayout) findViewById(R.id.loInnerRight);
 
             WebSettings webSettings = myWebView.getSettings();
             webSettings.setJavaScriptEnabled(true);
 
             myWebView.setWebChromeClient(new WebChromeClient() {
                 public void onProgressChanged(WebView view, int progress) {
-                    if (progress < 100 && Pbar.getVisibility() == ProgressBar.GONE) {
-                        Pbar.setVisibility(ProgressBar.VISIBLE);
-                        txtview.setVisibility(View.VISIBLE);
+                    if (progress < 100 && loMain.getVisibility() == ProgressBar.GONE) {
+                        loMain.setVisibility(ProgressBar.VISIBLE);
                     }
-                    Pbar.setProgress(progress);
+                    LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+                    llp.weight = progress;
+                    loLeft.setLayoutParams(llp);
+
+                    llp.weight = 100 - progress;
+                    loRight.setLayoutParams(llp);
                     if (progress == 100) {
-                        Pbar.setVisibility(ProgressBar.GONE);
-                        txtview.setVisibility(View.VISIBLE);
+                        loMain.setVisibility(ProgressBar.GONE);
                     }
                 }
             });
